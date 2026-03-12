14ed5ec01eff8add897638023688bcaf90826daa
I'm Your Boy Roy <V1x0r87@gmail.com>
2026-03-12 01:40:47 -0600

    Bump version to 0.2.4

--- src/ollamatoolkit/__init__.py
+++ src/ollamatoolkit/__init__.py
@@ -12,7 +12,7 @@ Quick Start:
     response = client.generate("llama2", "Hello!")
 """
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 # Core Components
 from .agents.simple import SimpleAgent, AgentHooks
